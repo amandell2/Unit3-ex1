1268872912612adb2a29c106133dc6ef53ba9af8
Amandell <amanda.mandell2@gmail.com>
2021-12-13 18:49:46 -0500

    changes made after tutorial

--- U3ex1script.js
+++ U3ex1script.js
@@ -5,9 +5,9 @@ Create a lastName variable. Set it to your last name.
 Create a fullName variable. Set it by concatenating firstName and lastName with a space between. (Or use a template literal.) (Don’t know what concatenate means? Google “JavaScript concatenate.”)
 Log the fullName variable to the console.
 */
-const firstName = "Amanda";
-const lastName = "Mandell";
-const fullName = firstName.concat(lastName);
+let firstName = "Amanda";
+let lastName = "Mandell";
+fullName = firstName.concat(" ", lastName);
 console.log(fullName);
 
 /*
@@ -15,8 +15,9 @@ Create a population variable. Set it to the population of Detroit or your favori
 Log population to the console.
 */
 
-const population = "674841";
+population = "674841";
 console.log(population);
+console.log(population * 3);
 
 
 /* Multiply Population by 3 & log to console */
@@ -28,12 +29,12 @@ Write a for loop that counts from 1 to 5 and logs each number to the console.
 Write a for loop that counts from 0 to 4 and logs each number to the console.
 */
 
-const javaScriptIsCool = "true";
-const colors = "red, orange, yellow, green, blue, purple";
+javaScriptIsCool = "true";
+colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 for (i=1; i<6; i++){
-
+    console.log(i);
 }
 
 for (i=0; i<5; i++){
-
+    console.log(i);
 }

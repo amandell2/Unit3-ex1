969650cd9137be5cfc2f69103c3b32a90826b5fd
Amandell <amanda.mandell2@gmail.com>
2021-12-13 18:29:15 -0500

    initial commit

---
+++ U3ex1script.js
@@ -0,0 +1,39 @@
+
+/*
+Create a firstName variable. Set it to your first name.
+Create a lastName variable. Set it to your last name.
+Create a fullName variable. Set it by concatenating firstName and lastName with a space between. (Or use a template literal.) (Don’t know what concatenate means? Google “JavaScript concatenate.”)
+Log the fullName variable to the console.
+*/
+const firstName = "Amanda";
+const lastName = "Mandell";
+const fullName = firstName.concat(lastName);
+console.log(fullName);
+
+/*
+Create a population variable. Set it to the population of Detroit or your favorite city.
+Log population to the console.
+*/
+
+const population = "674841";
+console.log(population);
+
+
+/* Multiply Population by 3 & log to console */
+
+/*
+Create a javaScriptIsCool variable. Set it to the boolean value true.
+Create a colors variable. Set it to an array of the colors of the rainbow. (Each color is a string.)
+Write a for loop that counts from 1 to 5 and logs each number to the console.
+Write a for loop that counts from 0 to 4 and logs each number to the console.
+*/
+
+const javaScriptIsCool = "true";
+const colors = "red, orange, yellow, green, blue, purple";
+for (i=1; i<6; i++){
+
+}
+
+for (i=0; i<5; i++){
+
+}
